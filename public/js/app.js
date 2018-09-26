b64899bfc5346bc006541603e5e445f309c53933
Jahnava <jahnavalmt@gmail.com>
2018-09-26 14:32:52 -0400

    added proper placement to the active links

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ $(function () {
     $(".animated-text").removeClass("hidden-text");
   }, 500);
 });
+
+
+<!-- Start of HubSpot Embed Code -->
+  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4858798.js"></script>
+<!-- End of HubSpot Embed Code -->
